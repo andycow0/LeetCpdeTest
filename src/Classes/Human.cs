3b4f432eb8130cf8f0ebe195025eb3e5e7c07c06
Unknown <andycow0@gmail.com>
2018-08-01 23:39:49 +0800

    Human example

---
+++ src/Classes/Human.cs
@@ -0,0 +1,9 @@
+﻿namespace src.Classes {
+    public class Human {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Human () {
+
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using LeetCodeTest.ArrayTest;
 using LeetCpdeTest.Questions.Numers;
+using src.Classes;
 using src.Questions.LinkedListTest;
 using src.Questions.Numers;
 using src.Questions.StringTest;
@@ -12,6 +13,19 @@ namespace LeetCodeTest {
             // var target = 9;
             // var twoSumResult = TwoSum_1.TwoSum(twoSumNums, 9);
             // var z = "PAYPALISHIRING";
+            var humanA = new Human () { Age = 10, Name = "Andy" };
+            var humanB = new Human () { Age = 20, Name = "Ning" };
+            Console.WriteLine (humanA == humanB);
+
+            humanA = humanB;
+            Console.WriteLine (humanA == humanB);
+            Console.WriteLine ($"HumanA age is {humanA.Age}, HumanB age is {humanB.Age}");
+            Console.WriteLine ($"HumanA Name is {humanA.Name}, HumanB age is {humanB.Name}");
+
+            humanA.Age = 28;
+            humanA.Name = "Test";
+            Console.WriteLine ($"HumanA age is {humanA.Age}, HumanB age is {humanB.Age}");
+            Console.WriteLine ($"HumanA Name is {humanA.Name}, HumanB age is {humanB.Name}");
 
             // ListNode listNode = new ListNode (4) {
             //     next = new ListNode (5) {
@@ -21,23 +35,23 @@ namespace LeetCodeTest {
             //     }
             // };
             // var aa = DeleteNodeList_203.RemoveElements (listNode, 9);
-            var isPowerOfTwo = PowerofTwo_231.IsPowerOfTwo (10);
-            MajorityElement_169.MajorityElement (new int[] { 1 });
-            ValidAnagram_242.IsAnagram ("anagram", "nagaram");
+            // var isPowerOfTwo = PowerofTwo_231.IsPowerOfTwo (10);
+            // MajorityElement_169.MajorityElement (new int[] { 1 });
+            // ValidAnagram_242.IsAnagram ("anagram", "nagaram");
 
-            var nums = new int[] { };
+            // var nums = new int[] { };
 
             // var dupl = ContainsDuplicate_217.ContainsDuplicate(nums);
 
             // var addDigis = AddDigi258.AddDigits(38);
 
-            var a = "THIS";
-            Console.WriteLine (a.Length);
-            Console.WriteLine (ArrayObj.ToLowerCaseString (a));
+            // var a = "THIS";
+            // Console.WriteLine (a.Length);
+            // Console.WriteLine (ArrayObj.ToLowerCaseString (a));
 
-            var s = "This is a test.";
-            var t = ", and this is as well.";
-            Console.WriteLine (String.Join (s, t.ToCharArray ()));
+            // var s = "This is a test.";
+            // var t = ", and this is as well.";
+            // Console.WriteLine (String.Join (s, t.ToCharArray ()));
 
             Console.ReadLine ();
         }
