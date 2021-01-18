bc99b52f18fca85824748db4aa265bf62e0f40a4
Unknown <andycow0@gmail.com>
2021-01-19 00:26:35 +0800

    1678 - Goal Parser Interpretation

--- src/Questions/ArrayTest/ArrayTest.cs
+++ src/Questions/ArrayTest/ArrayTest.cs
@@ -1,53 +1,39 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace src.Questions.ArrayTest
-{
-    public class ArrayTest
-    {
-        public static void DuplicateZeros(int[] arr)
-        {
-            if (!(arr.Length >= 1 && arr.Length <= 1000))
-            {
+namespace src.Questions.ArrayTest {
+    public class ArrayTest {
+        public static void DuplicateZeros (int[] arr) {
+            if (!(arr.Length >= 1 && arr.Length <= 1000)) {
                 return;
             }
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] < 0 || arr[i] > 9)
-                {
+            for (int i = 0; i < arr.Length; i++) {
+                if (arr[i] < 0 || arr[i] > 9) {
                     return;
                 }
             }
 
             var newIdx = 0;
             var idx = 0;
-            while (idx < arr.Length)
-            {
-                if (arr[idx] == 0)
-                {
+            while (idx < arr.Length) {
+                if (arr[idx] == 0) {
                     arr[idx] = arr[idx + 1];
                     newIdx += 2;
-                }
-                else
-                {
+                } else {
                     newIdx++;
                 }
             }
         }
 
-        public static int NumberofGoodPairs(int[] nums)
-        {
+        public static int NumberofGoodPairs (int[] nums) {
             var result = 0;
 
             if (nums.Length < 1) return result;
 
-            for (int i = 0; i < nums.Length; i++)
-            {
-                for (int j = nums.Length - 1; j > i; j--)
-                {
-                    if (nums[i] == nums[j])
-                    {
+            for (int i = 0; i < nums.Length; i++) {
+                for (int j = nums.Length - 1; j > i; j--) {
+                    if (nums[i] == nums[j]) {
                         result++;
                     }
                 }
@@ -56,17 +42,14 @@ namespace src.Questions.ArrayTest
             return result;
         }
 
-        public static int[] Shuffle(int[] nums, int n)
-        {
-            if (n < 1 || nums.Length % n != 0)
-            {
+        public static int[] Shuffle (int[] nums, int n) {
+            if (n < 1 || nums.Length % n != 0) {
                 return null;
             }
 
             var result = new int[nums.Length];
 
-            for (var i = 0; i < n; i++)
-            {
+            for (var i = 0; i < n; i++) {
                 result[i * 2] = nums[i];
                 result[i * 2 + 1] = nums[i + n];
             }
@@ -74,34 +57,50 @@ namespace src.Questions.ArrayTest
             return result;
         }
 
-        public static string LongestCommonPrefix(string[] strs)
-        {
+        public static string LongestCommonPrefix (string[] strs) {
             var strsLength = strs.Length;
 
-            var hasTooLongPrefixLength = strs.Any(s => s.Length > 200);
+            var hasTooLongPrefixLength = strs.Any (s => s.Length > 200);
 
-            if (strsLength < 1 || strsLength > 200 || hasTooLongPrefixLength)
-            {
+            if (strsLength < 1 || strsLength > 200 || hasTooLongPrefixLength) {
                 return null;
             }
 
             var result = strs[0];
 
-            for (int i = 1; i < strs.Length; i++)
-            {
-                while (strs[i].IndexOf(result) != 0)
-                {
-                    result = result.Substring(0, result.Length - 1);
-                    if (result.Length < 1)
-                    {
+            for (int i = 1; i < strs.Length; i++) {
+                while (strs[i].IndexOf (result) != 0) {
+                    result = result.Substring (0, result.Length - 1);
+                    if (result.Length < 1) {
                         return string.Empty;
                     }
                 }
             }
 
-
-
             return result;
+        }
+
+        public static string Interpret (string command) {
+
+            var result = new List<string> ();
+            
+            var idx = 0;
+            
+            while (idx < command.Length) {
+                if (command[idx] == 'G') {
+                    idx++;
+                    result.Add("G");
+                } else if (command[idx] == '(' && command[idx+1] == ')') {
+                    idx += 2;
+                    result.Add("o");
+                } else {
+                    idx += 4;
+                    result.Add("al");
+                }
+                
+            }
+
+            return string.Join (string.Empty, result);
         }
     }
 }
--- tests/Questions/ArrayTests.cs
+++ tests/Questions/ArrayTests.cs
@@ -1,67 +1,77 @@
-namespace tests.Questions
-{
+namespace tests.Questions {
     using NUnit.Framework;
     using src.Questions.ArrayTest;
 
-    public class ArrayTests
-    {
+    public class ArrayTests {
         [Test]
-        public void DuplicateZerosTest()
-        {
+        public void DuplicateZerosTest () {
 
             var target = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
             // var target = new int[] { 8,4,5,0,0,0,0,7 };
             // var target = new int[] { 1, 2, 3 };
             var expected = false;
 
-            ArrayTest.DuplicateZeros(target);
+            ArrayTest.DuplicateZeros (target);
 
-            Assert.AreEqual(target, expected);
+            Assert.AreEqual (target, expected);
         }
 
         [Test]
-        public void NumberofGoodPairsTest()
-        {
+        public void NumberofGoodPairsTest () {
 
             var target = new int[] { 1, 2, 3, 1, 1, 3 };
 
             var expected = 4;
 
-            var actual = ArrayTest.NumberofGoodPairs(target);
+            var actual = ArrayTest.NumberofGoodPairs (target);
 
-            Assert.AreEqual(actual, expected);
+            Assert.AreEqual (actual, expected);
         }
 
         // Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
         // Return the array in the form [x1,y1,x2,y2,...,xn,yn].
         // nums = null or num length <> 2n -> null.
         [Test]
-        public void ShuffleTheArray_1470_Test()
-        {
+        public void ShuffleTheArray_1470_Test () {
             var target = new int[] { 2, 5, 1, 3, 4, 7 };
 
             var n = 3;
 
             var expected = new int[] { 2, 3, 5, 4, 1, 7 };
 
-            var actual = ArrayTest.Shuffle(target, n);
+            var actual = ArrayTest.Shuffle (target, n);
 
-            Assert.AreEqual(actual, expected);
+            Assert.AreEqual (actual, expected);
         }
 
         // Write a function to find the longest common prefix string amongst an array of strings.
         // If there is no common prefix, return an empty string "".
-
         [Test]
-        public void LongestCommonPrefix_Test()
-        {
+        public void LongestCommonPrefix_Test () {
             var target = new string[] { "flower", "flow", "flight" };
 
             var expected = "fl";
 
-            var actual = ArrayTest.LongestCommonPrefix(target);
+            var actual = ArrayTest.LongestCommonPrefix (target);
 
-            Assert.AreEqual(actual, expected);
+            Assert.AreEqual (actual, expected);
+        }
+
+        // You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+        //Given the string command, return the Goal Parser's interpretation of command.
+        [Test]
+        public void GoalParserInterpretation_Test () {
+
+             var target = "G()(al)";
+            // var target = "G()()()()(al)";
+            //var target = "G()()()(al)()";
+            var expected = "Goal";
+            // var expected = "Gooooal";
+            // var expected = "Goooalo";
+
+            string actual = ArrayTest.Interpret (target);
+
+            Assert.AreEqual (actual, expected);
         }
     }
 }
