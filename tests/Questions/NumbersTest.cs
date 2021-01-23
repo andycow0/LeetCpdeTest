c6aba96cc05eb415792e0f9794c16c0958e5bf47
Andy.Kao <andy.kao@garmin.com>
2021-01-23 13:14:31 +0800

    412. Fizz Buzz

--- tests/Questions/NumbersTest.cs
+++ tests/Questions/NumbersTest.cs
@@ -1,8 +1,9 @@
+using LeetCodeTest.Questions;
 using NUnit.Framework;
 using src.Questions.Numers;
 
 namespace tests.Questions {
-    public class NumbersTest {
+    public class NumbersTests {
         [Test]
         public void PowerOfThree_326Test () {
             var expected = false;
@@ -19,6 +20,22 @@ namespace tests.Questions {
             var acutal = PowerOfOneBit_191.HammingWeight (11);
 
             Assert.AreEqual (acutal, expected);
+        }
+
+        // 412. Fizz Buzz
+        // https://leetcode.com/problems/fizz-buzz/
+        // Write a program that outputs the string representation of numbers from 1 to n.
+        // But for multiples of three it should output ¡§Fizz¡¨ instead of the number and for the multiples of five output ¡§Buzz¡¨. For numbers which are multiples of both three and five output ¡§FizzBuzz¡¨.
+        [Test, Category("NumbersTest")]
+        public void FizzBuzzTest()
+        {
+            // arrange
+            var n = 15;
+            var expected = false;
+            // actual
+            var actual = NumbersTest.FizzBuzz(n);
+            // assert
+            Assert.AreEqual(actual, expected);
         }
     }
 }
