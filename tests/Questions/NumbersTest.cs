59706d3c16c23de64ed2fcbd1da9fe08f81a147b
Unknown <andycow0@gmail.com>
2021-02-07 23:43:15 +0800

    7. Reverse Integer

--- tests/Questions/NumbersTest.cs
+++ tests/Questions/NumbersTest.cs
@@ -57,5 +57,20 @@ namespace tests.Questions
             // assert
             Assert.AreEqual(actual, expected);
         }
+
+        // 7. Reverse Integer
+        // https://leetcode.com/problems/reverse-integer/
+        // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+        [Test, Category("NumbersTest")]
+        public void ReverseTest()
+        {
+            // arrange
+            var n = -2147483648;
+            var expected = 321;
+            // actual
+            var actual = NumbersTest.Reverse(n);
+            // assert
+            Assert.AreEqual(actual, expected);
+        }
     }
 }
