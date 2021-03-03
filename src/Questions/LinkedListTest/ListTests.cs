52c8b1c2615bb99174b1afba3ab379eb9cb1c805
Unknown <andycow0@gmail.com>
2021-03-04 00:17:30 +0800

    21. Merge Two Sorted Lists

--- src/Questions/LinkedListTest/ListTests.cs
+++ src/Questions/LinkedListTest/ListTests.cs
@@ -2,33 +2,90 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LeetCodeTest.Questions.LinkedListTest
-{
-    public class ListTest
-    {
-        public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
-        {
-            var result = new ListNode(0);
+namespace LeetCodeTest.Questions.LinkedListTest {
+    public class ListTest {
+        public static ListNode GetIntersectionNode (ListNode headA, ListNode headB) {
 
-            //var arrA = GetArray(headA);
-            //var arrB = GetArray(headB);
+            var lengthA = GetLength (headA);
+            var lengthB = GetLength (headB);
+            ListNode result = null;
+            var diff = lengthA - lengthB;
+            var skip = Math.Abs (diff);
 
-            while (headA.next != null)
-            {
-                if (headA.val == headB.val)
-                {
-                    result.val = headA.val;
+            if (diff < 0) {
+                while (skip > 0) {
+                    headB = headB.next;
+                    skip--;
+                }
+            } else {
+                while (skip > 0) {
+                    headA = headA.next;
+                    skip--;
+                }
+            }
+
+            while (headA != null) {
+                if (headA.val == headB.val) {
+                    result = headA;
                     break;
                 }
+                headB = headB.next;
                 headA = headA.next;
             }
 
             return result;
         }
 
-        //private object GetArray(ListNode headA)
-        //{
+        private static int GetLength (ListNode listNode) {
+            var result = 0;
+            while (listNode != null) {
+                result++;
+                listNode = listNode.next;
+            }
+            return result;
+        }
 
-        //}
+        private static int[] GetNewArray (ListNode listNode) {
+            var result = new List<int> ();
+            while (listNode != null) {
+                result.Add (listNode.val);
+                listNode = listNode.next;
+            }
+            return result.ToArray ();
+        }
+
+        public static ListNode MergeTwoLists (ListNode l1, ListNode l2) {
+
+            if (l1 == null || l2 == null) {
+                return null;
+            }
+
+            ListNode result = null;
+
+            while (l1 != null) {
+
+                while (l2 != null) {
+                    if (l1.val > l2.val) {
+                        if (result is null) {
+                            result = new ListNode (l2.val);
+                        } else {
+                            result.next = l2;
+                        }
+                        break;
+                    } else {
+                        if (result is null) {
+                            result = new ListNode (l1.val);
+                        } else {
+                            result.next = l1;
+                        }
+                    }
+                    l2 = l2.next;
+                }
+                
+                l1 = l1.next;
+            }
+
+            return result;
+        }
     }
 }
--- tests/Questions/ListTests.cs
+++ tests/Questions/ListTests.cs
@@ -1,31 +1,58 @@
 using LeetCodeTest.Questions.LinkedListTest;
 using NUnit.Framework;
 
-namespace tests.Questions
-{
-    public class ListTests
-    {
-        [Test, Category("ListTest")]
-        public void GetIntersectionNodeTest()
-        {
-            var headA = new ListNode(4);
-            headA.next = new ListNode(1);
-            headA.next.next = new ListNode(8);
-            headA.next.next.next = new ListNode(4);
-            headA.next.next.next.next = new ListNode(5);
+namespace tests.Questions {
+    public class ListTests {
+        // 160. Intersection of Two Linked Lists
+        // https://leetcode.com/problems/intersection-of-two-linked-lists/submissions/
+        [Test, Category ("ListTest")]
+        public void GetIntersectionNodeTest () {
+            // var headA = new ListNode(4);
+            // headA.next = new ListNode(1);
+            // headA.next.next = new ListNode(8);
+            // headA.next.next.next = new ListNode(4);
+            // headA.next.next.next.next = new ListNode(5);
 
-            var headB = new ListNode(5);
-            headB.next = new ListNode(6);
-            headB.next.next = new ListNode(1);
-            headB.next.next.next = new ListNode(8);
-            headB.next.next.next.next = new ListNode(4);
-            headB.next.next.next.next.next = new ListNode(5);
+            // var headB = new ListNode(5);
+            // headB.next = new ListNode(6);
+            // headB.next.next = new ListNode(1);
+            // headB.next.next.next = new ListNode(8);
+            // headB.next.next.next.next = new ListNode(4);
+            // headB.next.next.next.next.next = new ListNode(5);
 
-            var expected = 2;
+            var headA = new ListNode (8);
+
+            var headB = new ListNode (4);
+            headB.next = new ListNode (1);
+            headB.next.next = new ListNode (8);
+            headB.next.next.next = new ListNode (4);
+            headB.next.next.next.next = new ListNode (5);
+
+            //var expected = 2;
             // actual
-            var actual = ListTest.GetIntersectionNode(headA, headB);
+            var actual = ListTest.GetIntersectionNode (headA, headB);
             // assert
             //Assert.AreEqual(actual, expected);
         }
+
+        // 21. Merge Two Sorted Lists
+        // https://leetcode.com/problems/merge-two-sorted-lists/
+        [Test, Category ("ListTest")]
+        public void MergeTwoListsTest () {
+
+            var l1 = new ListNode (1);
+            l1.next = new ListNode (2);
+            l1.next.next = new ListNode (4);
+
+            var l2 = new ListNode (1);
+            l2.next = new ListNode (3);
+            l2.next.next = new ListNode (4);
+
+            // actual
+            var actual = ListTest.MergeTwoLists (l1, l2);
+            // assert
+            //Assert.AreEqual(actual, expected);
+        }
+
     }
 }
