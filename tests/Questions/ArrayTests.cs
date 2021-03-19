9eaca28bc3017cc2eb92f06d63141eb781903ceb
Andy.Kao <andy.kao@garmin.com>
2021-03-19 15:22:55 +0800

    287. Find the Duplicate Number

--- tests/Questions/ArrayTests.cs
+++ tests/Questions/ArrayTests.cs
@@ -1,66 +1,73 @@
-namespace tests.Questions {
+namespace tests.Questions
+{
     using NUnit.Framework;
     using src.Questions.ArrayTest;
 
-    public class ArrayTests {
+    public class ArrayTests
+    {
         [Test]
-        public void DuplicateZerosTest () {
+        public void DuplicateZerosTest()
+        {
 
             var target = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
             // var target = new int[] { 8,4,5,0,0,0,0,7 };
             // var target = new int[] { 1, 2, 3 };
             var expected = false;
 
-            ArrayTest.DuplicateZeros (target);
+            ArrayTest.DuplicateZeros(target);
 
-            Assert.AreEqual (target, expected);
+            Assert.AreEqual(target, expected);
         }
 
         [Test]
-        public void NumberofGoodPairsTest () {
+        public void NumberofGoodPairsTest()
+        {
 
             var target = new int[] { 1, 2, 3, 1, 1, 3 };
 
             var expected = 4;
 
-            var actual = ArrayTest.NumberofGoodPairs (target);
+            var actual = ArrayTest.NumberofGoodPairs(target);
 
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
         // Return the array in the form [x1,y1,x2,y2,...,xn,yn].
         // nums = null or num length <> 2n -> null.
         [Test]
-        public void ShuffleTheArray_1470_Test () {
+        public void ShuffleTheArray_1470_Test()
+        {
             var target = new int[] { 2, 5, 1, 3, 4, 7 };
 
             var n = 3;
 
             var expected = new int[] { 2, 3, 5, 4, 1, 7 };
 
-            var actual = ArrayTest.Shuffle (target, n);
+            var actual = ArrayTest.Shuffle(target, n);
 
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // Write a function to find the longest common prefix string amongst an array of strings.
         // If there is no common prefix, return an empty string "".
         [Test]
-        public void LongestCommonPrefix_Test () {
+        public void LongestCommonPrefix_Test()
+        {
             var target = new string[] { "flower", "flow", "flight" };
 
             var expected = "fl";
 
-            var actual = ArrayTest.LongestCommonPrefix (target);
+            var actual = ArrayTest.LongestCommonPrefix(target);
 
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
         //Given the string command, return the Goal Parser's interpretation of command.
         [Test]
-        public void GoalParserInterpretation_Test () {
+        public void GoalParserInterpretation_Test()
+        {
 
             var target = "G()(al)";
             // var target = "G()()()()(al)";
@@ -69,37 +76,39 @@ namespace tests.Questions {
             // var expected = "Gooooal";
             // var expected = "Goooalo";
 
-            string actual = ArrayTest.Interpret (target);
+            string actual = ArrayTest.Interpret(target);
 
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
         //Given the string command, return the Goal Parser's interpretation of command.
         [Test]
-        public void NumJewelsInStones_Test () {
+        public void NumJewelsInStones_Test()
+        {
 
             var jewels = "aA";
             var stones = "aAAbbbb";
             var expected = 3;
 
-            int actual = ArrayTest.NumJewelsInStones (jewels, stones);
+            int actual = ArrayTest.NumJewelsInStones(jewels, stones);
 
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
         // 1480. Running Sum of 1d Array
         // https://leetcode.com/problems/running-sum-of-1d-array/
         // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
         // Return the running sum of nums.
         [Test]
-        public void RunningSum_Test () {
+        public void RunningSum_Test()
+        {
             // arrange
             var nums = new int[4] { 1, 2, 3, 4 };
             var expected = new int[4] { 1, 3, 6, 10 };
             // actual
-            var actual = ArrayTest.RunningSum (nums);
+            var actual = ArrayTest.RunningSum(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 1528. Shuffle String
@@ -107,15 +116,16 @@ namespace tests.Questions {
         // The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
         // Return the shuffled string.
         [Test]
-        public void ShuffleString_Test () {
+        public void ShuffleString_Test()
+        {
             // arrange
             var s = "codeleet";
             var indices = new int[] { 4, 5, 6, 7, 0, 2, 1, 3 };
             var expected = "leetcode";
             // actual
-            var actual = ArrayTest.ShuffleString (s, indices);
+            var actual = ArrayTest.ShuffleString(s, indices);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 344. Reverse String
@@ -123,14 +133,15 @@ namespace tests.Questions {
         // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
         // You may assume all the characters consist of printable ascii characters.
         [Test]
-        public void ReverseString_Test () {
+        public void ReverseString_Test()
+        {
             // arrange
             var s = new char[] { 'h', 'e', 'l', 'l', 'o' };
             var expected = new char[] { 'o', 'l', 'l', 'e', 'h' };
             // actual
-            var actual = ArrayTest.ReverseString (s);
+            var actual = ArrayTest.ReverseString(s);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 136. Single Number
@@ -138,14 +149,15 @@ namespace tests.Questions {
         // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
         // Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
         [Test]
-        public void SingleNumber_Test () {
+        public void SingleNumber_Test()
+        {
             // arrange
             var nums = new int[] { 1, 1, 2, 3, 4, 5, 4, 5, 3, -1, 2 };
             var expected = 1;
             // actual
-            int actual = ArrayTest.SingleNumber (nums);
+            int actual = ArrayTest.SingleNumber(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 121. Best Time to Buy and Sell Stock
@@ -153,96 +165,103 @@ namespace tests.Questions {
         // You are given an array prices where prices[i] is the price of a given stock on the ith day.
         //You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
         //Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.
-        [Test, Category ("ArrayTests")]
-        public void MaxProfitr_Test () {
+        [Test, Category("ArrayTests")]
+        public void MaxProfitr_Test()
+        {
             // arrange
             var nums = new int[] { 7, 1, 5, 3, 6, 4 };
             //var nums = new int[] { 7, 6, 4, 3, 1 };
             var expected = 5;
             //var expected = 0;
             // actual
-            int actual = ArrayTest.MaxProfit (nums);
+            int actual = ArrayTest.MaxProfit(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
         // 217. Contains Duplicate
         // https://leetcode.com/problems/contains-duplicate/
         // Given an array of integers, find if the array contains any duplicates.
         //Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
         [Test]
-        public void ContainsDuplicate_Test () {
+        public void ContainsDuplicate_Test()
+        {
             // arrange
             var nums = new int[] { 1, 2, 3, 1 };
             var expected = true;
             // var nums = new int[] { 1,2,3,4};
             // var expected = false;
             // actual
-            bool actual = ArrayTest.ContainsDuplicate (nums);
+            bool actual = ArrayTest.ContainsDuplicate(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 26. Remove Duplicates from Sorted Array
         // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
         [Test]
-        public void RemoveDuplicatesTest () {
+        public void RemoveDuplicatesTest()
+        {
             // arrange
             var nums = new int[] { 1, 1, 2 };
             var expected = 2;
 
             // actual
-            int actual = ArrayTest.RemoveDuplicates (nums);
+            int actual = ArrayTest.RemoveDuplicates(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 53. Maximum Subarray
         // https://leetcode.com/problems/maximum-subarray/
         // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
         [Test]
-        public void MaxSubArrayTest () {
+        public void MaxSubArrayTest()
+        {
             // arrange
-            var nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            var nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
             var expected = 6;
 
             // actual
-            int actual = ArrayTest.MaxSubArray (nums);
+            int actual = ArrayTest.MaxSubArray(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 448. Find All Numbers Disappeared in an Array
         // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
         [Test]
-        public void FindDisappearedNumbersTest () {
+        public void FindDisappearedNumbersTest()
+        {
             // arrange
             var nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
             var expected = new int[] { 5, 6 };
 
             // actual
-            int[] actual = ArrayTest.FindDisappearedNumbers (nums);
+            int[] actual = ArrayTest.FindDisappearedNumbers(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 442. Find All Duplicates in an Array
         // https://leetcode.com/problems/find-all-duplicates-in-an-array/
         [Test]
-        public void FindDuplicatesTest () {
+        public void FindDuplicatesTest()
+        {
             // arrange
             var nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
             var expected = new int[] { 2, 3 };
 
             // actual
-            var actual = ArrayTest.FindDuplicates (nums);
+            var actual = ArrayTest.FindDuplicates(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 88. Merge Sorted Array
         // https://leetcode.com/problems/merge-sorted-array/
         [Test]
-        public void MergeTest () {
+        public void MergeTest()
+        {
             // arrange
             // var nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
             // var nums2 = new int[] { 2, 5, 6 };
@@ -252,28 +271,30 @@ namespace tests.Questions {
 
             // actual
             //ArrayTest.Merge (nums1, 3, nums2, 3);
-            ArrayTest.Merge (nums1, 0, nums2, 1);
+            ArrayTest.Merge(nums1, 0, nums2, 1);
             // assert
-            Assert.AreEqual (nums1, expected);
+            Assert.AreEqual(nums1, expected);
         }
 
         // 268. Missing Number
         // https://leetcode.com/problems/missing-number/
-        [Test, Category ("ArrayTest")]
-        public void MissingNumberTest () {
+        [Test, Category("ArrayTest")]
+        public void MissingNumberTest()
+        {
             // arrange
             var nums = new int[] { 3, 0, 1 };
             var expected = 2;
             // actual
-            int actual = ArrayTest.MissingNumber (nums);
+            int actual = ArrayTest.MissingNumber(nums);
             // assert
-            Assert.AreEqual (actual, expected);
+            Assert.AreEqual(actual, expected);
         }
 
         // 989. Add to Array-Form of Integer
         // https://leetcode.com/problems/add-to-array-form-of-integer/
-        [Test, Category ("ArrayTest")]
-        public void AddToArrayFormTest () {
+        [Test, Category("ArrayTest")]
+        public void AddToArrayFormTest()
+        {
 
             // arrange
             var A = new int[] { 0 };
@@ -283,7 +304,7 @@ namespace tests.Questions {
             //var K = 1;
 
             // actual
-            var actual = ArrayTest.AddToArrayForm (A, K);
+            var actual = ArrayTest.AddToArrayForm(A, K);
 
             // assert
             // Assert.AreEqual (actual, expected);
@@ -291,8 +312,9 @@ namespace tests.Questions {
 
         // 350. Intersection of Two Arrays II
         // https://leetcode.com/problems/add-to-array-form-of-integer/
-        [Test, Category ("ArrayTest")]
-        public void IntersectTest () {
+        [Test, Category("ArrayTest")]
+        public void IntersectTest()
+        {
 
             // arrange
             // var nums1 = new int[] { 4, 9, 5 };
@@ -302,7 +324,7 @@ namespace tests.Questions {
             var nums1 = new int[] { 1, 2 };
             var nums2 = new int[] { 1, 1 };
             // actual
-            var actual = ArrayTest.Intersect (nums1, nums2);
+            var actual = ArrayTest.Intersect(nums1, nums2);
 
             // assert
             // Assert.AreEqual (actual, expected);
@@ -310,17 +332,34 @@ namespace tests.Questions {
 
         // 1002. Find Common Characters
         // https://leetcode.com/problems/find-common-characters/
-        [Test, Category ("ArrayTest")]
-        public void CommonCharsTest () {
+        [Test, Category("ArrayTest")]
+        public void CommonCharsTest()
+        {
 
             // arrange
             var A = new string[] { "bella", "label", "roller" };
-            
+
             // actual
-            var actual = ArrayTest.CommonChars (A);
+            var actual = ArrayTest.CommonChars(A);
 
             // assert
             // Assert.AreEqual (actual, expected);
+        }
+
+        // 287. Find the Duplicate Number
+        // https://leetcode.com/problems/find-the-duplicate-number/
+        [Test, Category("ArrayTest")]
+        public void FindDuplicateTest()
+        {
+            // arrange
+            var nums = new int[] { 1, 1 };
+            var expected = 2;
+
+            // actual
+            var actual = ArrayTest.FindDuplicate(nums);
+
+            // assert
+             Assert.AreEqual (actual, expected);
         }
     }
 }
