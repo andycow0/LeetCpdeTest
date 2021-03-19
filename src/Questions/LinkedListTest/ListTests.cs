65f84a44cc2b980e73878d61bee98a108de88a24
Andy.Kao <andy.kao@garmin.com>
2021-03-19 12:24:57 +0800

    141. Linked List Cycle

--- src/Questions/LinkedListTest/ListTests.cs
+++ src/Questions/LinkedListTest/ListTests.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LeetCodeTest.Questions.LinkedListTest {
-    public class ListTest {
-        public static ListNode GetIntersectionNode (ListNode headA, ListNode headB) {
+namespace LeetCodeTest.Questions.LinkedListTest
+{
+    public class ListTest
+    {
+        public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
 
-            while (headA != null) {
+            while (headA != null)
+            {
                 ListNode pB = headB;
-                while (pB != null) {
+                while (pB != null)
+                {
                     if (headA.val == pB.val)
                         return headA;
                     pB = pB.next;
@@ -18,33 +23,42 @@ namespace LeetCodeTest.Questions.LinkedListTest {
             return null;
         }
 
-        private static int GetLength (ListNode listNode) {
+        private static int GetLength(ListNode listNode)
+        {
             var result = 0;
-            while (listNode != null) {
+            while (listNode != null)
+            {
                 result++;
                 listNode = listNode.next;
             }
             return result;
         }
 
-        private static int[] GetNewArray (ListNode listNode) {
-            var result = new List<int> ();
-            while (listNode != null) {
-                result.Add (listNode.val);
+        private static int[] GetNewArray(ListNode listNode)
+        {
+            var result = new List<int>();
+            while (listNode != null)
+            {
+                result.Add(listNode.val);
                 listNode = listNode.next;
             }
-            return result.ToArray ();
+            return result.ToArray();
         }
 
-        public static ListNode MergeTwoLists (ListNode l1, ListNode l2) {
-            var result = new ListNode ();
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            var result = new ListNode();
             var current = result;
 
-            while (l1 != null || l2 != null) {
-                if (l1 == null || (l2 != null && l1.val > l2.val)) {
+            while (l1 != null || l2 != null)
+            {
+                if (l1 == null || (l2 != null && l1.val > l2.val))
+                {
                     current.next = l2;
                     l2 = l2.next;
-                } else {
+                }
+                else
+                {
                     current.next = l1;
                     l1 = l1.next;
                 }
@@ -54,12 +68,14 @@ namespace LeetCodeTest.Questions.LinkedListTest {
             return result.next;
         }
 
-        public static ListNode ReverseList (ListNode head) {
+        public static ListNode ReverseList(ListNode head)
+        {
 
             ListNode result = null;
-            while (head != null) {
+            while (head != null)
+            {
                 var last = result;
-                result = new ListNode (head.val);
+                result = new ListNode(head.val);
                 result.next = last;
 
                 head = head.next;
@@ -68,19 +84,23 @@ namespace LeetCodeTest.Questions.LinkedListTest {
             return result;
         }
 
-        public static bool IsPalindrome (ListNode head) {
+        public static bool IsPalindrome(ListNode head)
+        {
 
-            var list = new List<int> ();
+            var list = new List<int>();
 
-            while (head != null) {
-                list.Add (head.val);
+            while (head != null)
+            {
+                list.Add(head.val);
                 head = head.next;
             }
 
             var idx = 0;
             var count = list.Count;
-            while (idx < (count / 2 + 1)) {
-                if (list[idx] != list[count - idx - 1]) {
+            while (idx < (count / 2 + 1))
+            {
+                if (list[idx] != list[count - idx - 1])
+                {
                     return false;
                 }
                 idx++;
@@ -89,9 +109,10 @@ namespace LeetCodeTest.Questions.LinkedListTest {
             return true;
         }
 
-        public static ListNode AddTwoNumbers (ListNode l1, ListNode l2) {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
 
-            var result = new ListNode (0);
+            var result = new ListNode(0);
             var current = result;
 
             var sum1 = 0;
@@ -99,24 +120,47 @@ namespace LeetCodeTest.Questions.LinkedListTest {
             // var count = 1;
             var carry = 0;
 
-            while (l1 != null || l2 != null) {
+            while (l1 != null || l2 != null)
+            {
 
                 sum1 = l1 != null ? l1.val : 0;
                 sum2 = l2 != null ? l2.val : 0;
 
                 var sum = sum1 + sum2 + carry;
                 carry = sum / 10;
-                current.next = new ListNode (sum % 10);
+                current.next = new ListNode(sum % 10);
 
                 l1 = l1 is null ? null : l1.next;
                 l2 = l2 is null ? null : l2.next;
 
                 current = current.next;
             }
-            if (carry == 1) {
-                current.next = new ListNode (1);
+            if (carry == 1)
+            {
+                current.next = new ListNode(1);
             }
             return result.next;
+        }
+
+        public static bool HasCycle(ListNode head)
+        {
+            var list = new List<ListNode>();
+
+            while (head != null)
+            {
+                if (list.IndexOf(head) < 0)
+                {
+                    list.Add(head);
+                }
+                else
+                {
+                    return true;
+                }
+
+                head = head.next;
+            }
+
+            return false;
         }
     }
 }
--- tests/Questions/ListTests.cs
+++ tests/Questions/ListTests.cs
@@ -1,25 +1,28 @@
 using LeetCodeTest.Questions.LinkedListTest;
 using NUnit.Framework;
 
-namespace tests.Questions {
-    public class ListTests {
+namespace tests.Questions
+{
+    public class ListTests
+    {
         // 160. Intersection of Two Linked Lists
         // https://leetcode.com/problems/intersection-of-two-linked-lists/submissions/
-        [Test, Category ("ListTest")]
-        public void GetIntersectionNodeTest () {
+        [Test, Category("ListTest")]
+        public void GetIntersectionNodeTest()
+        {
 
-            var headA = new ListNode (4);
-            headA.next = new ListNode (1);
-            headA.next.next = new ListNode (8);
-            headA.next.next.next = new ListNode (4);
-            headA.next.next.next.next = new ListNode (5);
+            var headA = new ListNode(4);
+            headA.next = new ListNode(1);
+            headA.next.next = new ListNode(8);
+            headA.next.next.next = new ListNode(4);
+            headA.next.next.next.next = new ListNode(5);
 
-            var headB = new ListNode (5);
-            headB.next = new ListNode (6);
-            headB.next.next = new ListNode (1);
-            headB.next.next.next = new ListNode (8);
-            headB.next.next.next.next = new ListNode (4);
-            headB.next.next.next.next.next = new ListNode (5);
+            var headB = new ListNode(5);
+            headB.next = new ListNode(6);
+            headB.next.next = new ListNode(1);
+            headB.next.next.next = new ListNode(8);
+            headB.next.next.next.next = new ListNode(4);
+            headB.next.next.next.next.next = new ListNode(5);
 
             // var headA = new ListNode(8);
 
@@ -30,17 +33,18 @@ namespace tests.Questions {
             // headB.next.next.next.next = new ListNode(5);
 
             // actual
-            var actual = ListTest.GetIntersectionNode (headA, headB);
+            var actual = ListTest.GetIntersectionNode(headA, headB);
             // assert
             //Assert.AreEqual(actual, expected);
         }
 
         // 21. Merge Two Sorted Lists
         // https://leetcode.com/problems/merge-two-sorted-lists/
-        [Test, Category ("ListTest")]
-        public void MergeTwoListsTest () {
+        [Test, Category("ListTest")]
+        public void MergeTwoListsTest()
+        {
 
-            var l1 = new ListNode (1);
+            var l1 = new ListNode(1);
             //l1.next = new ListNode(2);
             //l1.next.next = new ListNode(4);
 
@@ -55,48 +59,51 @@ namespace tests.Questions {
             ListNode l2 = null;
 
             // actual
-            var actual = ListTest.MergeTwoLists (l1, l2);
+            var actual = ListTest.MergeTwoLists(l1, l2);
             // assert
             //Assert.AreEqual(actual, expected);
         }
 
         // 206. Reverse Linked List
         // https://leetcode.com/problems/reverse-linked-list/
-        [Test, Category ("ListTest")]
-        public void ReverseListTest () {
+        [Test, Category("ListTest")]
+        public void ReverseListTest()
+        {
 
-            var head = new ListNode (1);
-            head.next = new ListNode (2);
-            head.next.next = new ListNode (3);
-            head.next.next.next = new ListNode (4);
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
 
             // actual
-            var actual = ListTest.ReverseList (head);
+            var actual = ListTest.ReverseList(head);
             // assert
             //Assert.AreEqual(actual, expected);
         }
 
         // 234. Palindrome Linked List
         // https://leetcode.com/problems/palindrome-linked-list/
-        [Test, Category ("ListTest")]
-        public void IsPalindromeTest () {
+        [Test, Category("ListTest")]
+        public void IsPalindromeTest()
+        {
 
-            var head = new ListNode (1);
-            head.next = new ListNode (2);
-            head.next.next = new ListNode (3);
-            head.next.next.next = new ListNode (2);
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(2);
             // head.next.next.next.next = new ListNode(1);
 
             // actual
-            var actual = ListTest.IsPalindrome (head);
+            var actual = ListTest.IsPalindrome(head);
             // assert
             //Assert.AreEqual(actual, expected);
         }
 
         // 2. Add Two Numbers
         // https://leetcode.com/problems/add-two-numbers/submissions/
-        [Test, Category ("ListTest")]
-        public void AddTwoNumbersTest () {
+        [Test, Category("ListTest")]
+        public void AddTwoNumbersTest()
+        {
 
             // var l1 = new ListNode (2);
             // l1.next = new ListNode (4);
@@ -106,27 +113,46 @@ namespace tests.Questions {
             // l2.next = new ListNode (6);
             // l2.next.next = new ListNode (4);
 
-            var l1 = new ListNode (9);
-            l1.next = new ListNode (9);
-            l1.next.next = new ListNode (9);
-            l1.next.next.next = new ListNode (9);
-            l1.next.next.next.next = new ListNode (9);
-            l1.next.next.next.next.next = new ListNode (9);
-            l1.next.next.next.next.next.next = new ListNode (9);
+            var l1 = new ListNode(9);
+            l1.next = new ListNode(9);
+            l1.next.next = new ListNode(9);
+            l1.next.next.next = new ListNode(9);
+            l1.next.next.next.next = new ListNode(9);
+            l1.next.next.next.next.next = new ListNode(9);
+            l1.next.next.next.next.next.next = new ListNode(9);
 
-            var l2 = new ListNode (9);
-            l2.next = new ListNode (9);
-            l2.next.next = new ListNode (9);
-            l2.next.next.next = new ListNode (9);
+            var l2 = new ListNode(9);
+            l2.next = new ListNode(9);
+            l2.next.next = new ListNode(9);
+            l2.next.next.next = new ListNode(9);
 
             // var l1 = new ListNode (0);
             // var l2 = new ListNode (0);
 
             // actual
-            var actual = ListTest.AddTwoNumbers (l1, l2);
+            var actual = ListTest.AddTwoNumbers(l1, l2);
             // assert
             //Assert.AreEqual(actual, expected);
         }
 
+        // 141. Linked List Cycle
+        // https://leetcode.com/problems/linked-list-cycle/
+        [Test, Category("ListTest")]
+        public void HasCycleTest()
+        {
+            var head = new ListNode(3);
+            //head.next = new ListNode(2);
+            //head.next.next = new ListNode(0);
+            //head.next.next.next = new ListNode(9);
+            //head.next.next.next.next = new ListNode(9);
+            //head.next.next.next.next.next = new ListNode(9);
+            //head.next.next.next.next.next.next = new ListNode(9);
+
+
+            // actual
+            var actual = ListTest.HasCycle(head);
+            // assert
+            //Assert.AreEqual(actual, expected);
+        }
     }
 }
